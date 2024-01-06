8f7410088b3f15d7ac9ef4d70d2c2660588dae7f
Piyush Kumar <pk421780@gmail.com>
2024-01-06 08:04:30 +0530

    updated

--- Jan/6.cpp
+++ Jan/6.cpp
@@ -1,65 +1,68 @@
 #include <bits/stdc++.h>
 using namespace std;
-vector<int> dp;
-vector<vector<int>> arr;
-int n;
-int getNextIndex(vector<vector<int>> &arr, int l, int ele)
+class Solution
 {
-    int r = n - 1;
-    int indi = n;
-    while (l <= r)
+public:
+    vector<int> dp;
+    int n;
+    int getNextIndex(vector<vector<int>> &arr, int l, int tar)
     {
-        int mid = l + (r - l) / 2;
+        int r = n - 1;
 
-        if (arr[mid][0] >= ele)
+        int result = n + 1;
+
+        while (l <= r)
         {
-            int indi = mid;
-            r = mid - 1;
+            int mid = l + (r - l) / 2;
+
+            if (arr[mid][0] >= tar)
+            {
+                result = mid;
+                r = mid - 1;
+            }
+            else
+            {
+                l = mid + 1;
+            }
         }
-        else
+
+        return result;
+    }
+
+    int solve(vector<vector<int>> &arr, int i)
+    {
+        if (i >= n)
+            return 0;
+
+        if (dp[i] != -1)
+            return dp[i];
+
+        int next = getNextIndex(arr, i + 1, arr[i][1]);
+
+        int taken = arr[i][2] + solve(arr, next);
+        int notTaken = solve(arr, i + 1);
+
+        return dp[i] = max(taken, notTaken);
+    }
+
+    int jobScheduling(vector<int> &s, vector<int> &e, vector<int> &p)
+    {
+        n = s.size();
+
+        dp.resize(n + 1, -1);
+
+        vector<vector<int>> arr(n, vector<int>(3)); //{s, e, p}
+
+        for (int i = 0; i < n; i++)
         {
-            l = mid + 1;
+            arr[i] = {s[i], e[i], p[i]};
         }
+
+        sort(arr.begin(), arr.end());
+
+        return solve(arr, 0);
     }
-    return indi;
-}
-int rec(int i, vector<vector<int>> &arr)
-{
-
-    if (i >= n)
-    {
-        return 0;
-    }
-
-    if (dp[i] != -1)
-    {
-        return dp[i];
-    }
-
-    int notTake = rec(i + 1, arr);
-    int nextIndex = getNextIndex(arr, i + 1, arr[i][1]);
-    int take = rec(nextIndex, arr) + arr[i][2];
-
-    return dp[i] = max(take, notTake);
-}
-int jobScheduling(vector<int> &s, vector<int> &e, vector<int> &p)
-{
-    n = s.size();
-    dp.resize(n + 1, -1);
-    arr.resize(n, vector<int>(3));
-    for (int i = 0; i < n; i++)
-    {
-        arr[i] = {s[i], e[i], p[i]};
-    }
-
-    auto comp = [&](auto &vec1, auto &vec2)
-    {
-        return vec1[0] <= vec2[0];
-    };
-
-    sort(arr.begin(), arr.end(), comp);
-    return rec(0, arr);
-}
+};
 
 int main()
 {
