b6d41b9a24a99dfe15b2cceca9a41d259a69fe81
Piyush Kumar <pk421780@gmail.com>
2024-02-01 20:59:07 +0530

    1 feb potd is completed

--- Test/1.cpp
+++ Test/1.cpp
@@ -1,7 +1,22 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    cout<<"Test"<<endl;
+class Solution
+{
+public:
+    void rotate(vector<int> &nums, int k)
+    {
+        int n = nums.size();
+        k = k % n;
+
+        reverse(nums.begin(), nums.end());
+
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
+
+int main()
+{
+    cout << "Test" << endl;
 }
