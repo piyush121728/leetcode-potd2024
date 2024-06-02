3d79257062e778629789281cc9e8f10813d478c7
Piyush Kumar <pk421780@gmail.com>
2024-06-02 21:26:09 +0530

    2 june potd is completed.

---
+++ June/2.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    void reverseString(vector<char> &s)
+    {
+        reverse(s.begin(), s.end());
+    }
+};
+
+// link : https://leetcode.com/problems/reverse-string/?envType=daily-question&envId=2024-06-02
