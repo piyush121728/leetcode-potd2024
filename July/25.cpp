34b7584ee2e16780a0179e4b7a68e194626e469f
Piyush Kumar <pk421780@gmail.com>
2024-07-25 13:22:11 +0530

    25 july potd is completed.

---
+++ July/25.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> sortArray(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+        return nums;
+    }
+};
+
+// link : https://leetcode.com/problems/sort-an-array/?envType=daily-question&envId=2024-07-25
